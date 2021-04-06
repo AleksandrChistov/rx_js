9f39ef5f337cf9975705a6fecc742bd477a317b2
Alexander <aleksandrchistov.ru@yandex.ru>
2021-04-06 19:58:39 +0700

    Configured Webpack

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { interval, noop, Observable } from "rxjs";
+
+const sequence$ = new Observable((subscriber) => {
+    let count = 0;
+    const intervalId = setInterval(() => {
+        if (count !== 0 && count % 5 === 0) {
+            clearInterval(intervalId);
+            subscriber.complete();
+            return;
+        }
+        subscriber.next(count++);
+    }, 1000);
+
+    return {
+        unsubscribe: () => {
+            console.log('Unsubscribed');
+            clearInterval(intervalId);
+        }
+    }
+});
+
+const sub = sequence$.subscribe(
+    (value) => console.log(value),
+    noop,
+    () => console.log('Completed stream!')
+);
+
+setTimeout(() => sub.unsubscribe(), 3000);
+
+// -----------------------------------
+
+const sequence2$ = interval(1000);
+
+const sub1 = sequence2$.subscribe((value: number) => console.log('Sub1: ', value));
+const sub2 = sequence2$.subscribe((value: number) => console.log('Sub2: ', value));
+
+setTimeout(() => {
+    console.log('Sub1 is unsibscribed!');
+    sub1.unsubscribe();
+}, 3000);

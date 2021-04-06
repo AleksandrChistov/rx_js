a9256dd7e490a4231c0e1ceeeecf45adac76befe
Alexander <aleksandrchistov.ru@yandex.ru>
2021-04-06 14:57:05 +0700

    Why we need rxjs

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
 
-const sequence = new Promise(resolved => {
-    let count = 0;
-    setInterval(() => resolved(count++), 1000);
-});
+// const sequence = new Promise(resolved => {
+//     let count = 0;
+//     setInterval(() => resolved(count++), 1000);
+// });
 
-sequence.then(value => console.log(value));
-sequence.then(value => console.log(value));
+// sequence.then(value => console.log(value));
+// sequence.then(value => console.log(value));
+
+// const sequence = function* iteratorFn() {
+//     let item = 0;
+//     while (true) {
+//         yield item++;
+//     }
+// }();
+
+// console.log(sequence.next().value);
+// console.log(sequence.next().value);
+// console.log(sequence.next().value);
+// console.log(sequence.next().value);
+// console.log(sequence.next().value);
+// console.log(sequence.next().value);
+
+
+import { interval } from 'rxjs';
+
+interval(1000).subscribe(v => console.log(v));
